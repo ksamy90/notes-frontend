d92f9e7dc50bc1fc18a83beb04a1f4c42bb9ba28
samson <samson.omondi1@ibm.com>
2023-05-01 14:06:20 +0300

    refactor form to its own component

---
+++ src/components/Forms/LoginForm.js
@@ -0,0 +1,33 @@
+const LoginForm = ({
+  handleLogin,
+  username,
+  password,
+  setUsername,
+  setPassword,
+}) => {
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
